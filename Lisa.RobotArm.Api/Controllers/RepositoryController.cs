3c2d17e8169fab5d6143becb21864f5f7c2693ae
Bart Schop <Bart Schop>
2016-04-13 09:40:43 +0200

    Changed result to repository.Contents.

--- Lisa.RobotArm.Api/Controllers/RepositoryController.cs
+++ Lisa.RobotArm.Api/Controllers/RepositoryController.cs
@@ -9,11 +9,11 @@ namespace Lisa.RobotArm.Api.Controllers
         [HttpGet("{slug}")]
         public async Task<IActionResult> GetSingle(string slug)
         {
-            object repository = await TableStorage.GetLevel(slug, true);
+            dynamic repository = await TableStorage.GetLevel(slug, true);
 
             if (repository != null)
             {
-                return new HttpOkObjectResult(repository);
+                return new HttpOkObjectResult(repository.Contents);
             }
 
             return new HttpNotFoundResult();
